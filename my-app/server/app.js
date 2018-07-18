a33d64770e16c97c0345ae4cb9f1905830377b2e
JeromeLELONG <lelong@cnam.fr>
2018-07-18 13:26:52 -0700

    fixed corrector font size

--- my-app/server/app.js
+++ my-app/server/app.js
@@ -30,7 +30,7 @@ connection.query(
   throw error;
 }
 
-connection.
+
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
